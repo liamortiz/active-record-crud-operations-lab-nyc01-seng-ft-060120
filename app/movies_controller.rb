bcc1db4177e4e369b3d8493703303554272c7c73
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-11 21:05:21 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -38,8 +38,11 @@ def can_be_created_in_a_block(args = nil)
   end
   
   Movie.create do |m|
-    m.title = args.title
-    m.release_date = args.release_date
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.director = args[:director]
+    m.lead = args[:lead]
+    m.in_theaters = args[:in_theaters]
   end
 end
 

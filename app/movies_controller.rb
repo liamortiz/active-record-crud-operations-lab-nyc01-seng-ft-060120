b0613cf2090978b452d56dd207e8cc47a3a5b031
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-11 21:31:14 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -67,7 +67,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  Movie.order("release_date DESC").find_by("release_date > 2002")
+  Movie.order(release_date: "desc").find_by("release_date > 2002")
 end
 
 def can_be_found_updated_and_saved

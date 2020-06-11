ede434c3eade164d616d2e53605394d9c5d73cc0
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-11 21:22:00 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -32,17 +32,11 @@ def can_be_created_in_a_block(args = nil)
     args = {}
     args[:title] = "Home Alone"
     args[:release_date] = 1990
-    args[:director] = "Me"
-    args[:lead] = "Me"
-    args[:in_theaters] = true
   end
   
   Movie.create do |m|
     m.title = args[:title]
     m.release_date = args[:release_date]
-    m.director = args[:director]
-    m.lead = args[:lead]
-    m.in_theaters = args[:in_theaters]
   end
 end
 
